911fcf9149066da2c5438c07b02199646027b703
XiomaraPinosC <xiomytapinos@gmail.com>
2021-09-08 16:50:16 -0500

    conexion a BD remota

--- conexion.js
+++ conexion.js
@@ -1,10 +1,22 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
+  host: 'db4free.net',
+  user: 'xiomara',
+  password: 'Escuelita_29',
+  database: 'vestidorapp'
+});
+
+
+module.exports = connection;
+
+
+
+/*
+const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'prendasdb'
+  database: 'vestidorapp'
 });
-
-module.exports = connection;
+*/
